f3ac251b329eced526a3cbd27474af2a4eb867d2
kubiszr25 <kubiszrudolf@gmail.com>
2025-09-24 12:51:17 +0200

    test 8

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,6 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
-
-Console.WriteLine("Koľko máš rokov?");
+Console.WriteLine("Ahoj,Rudolf koľko máš rokov ?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Aha,máš teda 16 rokov.");
