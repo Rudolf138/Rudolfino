4cce7d2e2b68846d5e570984c247ba18cb40f218
kubiszr25 <kubiszrudolf@gmail.com>
2025-09-24 12:27:04 +0200

    test7

--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 const int Maxpoints = 100;
-const int MinAgeForDriversLicense = 17
+const int MinAgeForDriversLicense = 17;
 const string WelcomeMessage = "Vitajte v našej hre!";
 Console.WriteLine(Maxpoints);
 Console.WriteLine(MinAgeForDriversLicense);
--- src/AppsLab-007-Casting/Converter.cs
+++ src/AppsLab-007-Casting/Converter.cs
@@ -1,63 +1,65 @@
-﻿namespace AppsLab_007_Casting;
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppsLab_007_Casting;
 
 /// <summary>
 /// This class contains methods to convert between different data types.
 /// </summary>
 public class Converter
 {
-    /// <summary>
-    /// This method converts a string to an integer.
-    /// </summary>
-    public double IntToDouble(int number)
-    {
-        throw new NotImplementedException();
+	/// <summary>
+	/// This method converts a string to an integer.
+	/// </summary>
+	public double IntToDouble(int number)
+	{
+		return Convert.ToDouble(number);
+	}
+
+	/// <summary>
+	/// This method converts a double to an integer.
+	/// </summary>
+	public int DoubleToInt(double number)
+	{
+		return Convert.ToInt32(number);
     }
 
-    /// <summary>
-    /// This method converts a double to an integer.
-    /// </summary>
-    public int DoubleToInt(double number)
-    {
-        throw new NotImplementedException();
-    }
+	/// <summary>
+	/// This method converts a string to a double.
+	/// </summary>
+	public double StringToDouble(string number)
+	{
+		return Convert.ToDouble(number);
+	}
 
-    /// <summary>
-    /// This method converts a string to a double.
-    /// </summary>
-    public double StringToDouble(string number)
-    {
-        throw new NotImplementedException();
-    }
+	/// <summary>
+	/// This method converts a double to a string.
+	/// </summary>
+	public string DoubleToString(double number)
+	{
+		return Convert.ToString(number);
+	}
 
-    /// <summary>
-    /// This method converts a double to a string.
-    /// </summary>
-    public string DoubleToString(double number)
-    {
-        throw new NotImplementedException();
-    }
+	/// <summary>
+	/// This method converts a string to a boolean.
+	/// </summary>
+	public string IntToString(int number)
+	{
+		return Convert.ToString(number);
+	}
 
-    /// <summary>
-    /// This method converts a string to a boolean.
-    /// </summary>
-    public string IntToString(int number)
-    {
-        throw new NotImplementedException();
-    }
+	/// <summary>
+	/// This method converts a boolean to a string.
+	/// </summary>
+	public bool StringToBool(string boolValue)
+	{
+		return Convert.ToBoolean(boolValue);
+	}
 
     /// <summary>
     /// This method converts a boolean to a string.
     /// </summary>
-    public bool StringToBool(string boolValue)
-    {
-        throw new NotImplementedException();
-    }
-
-    /// <summary>
-    /// This method converts a boolean to a string.
-    /// </summary>
-    public string BoolToString(bool boolValue)
-    {
-        throw new NotImplementedException();
-    }
+	public string BoolToString(bool boolValue)
+	{
+		return Convert.ToString(boolValue);
+	}
 }
