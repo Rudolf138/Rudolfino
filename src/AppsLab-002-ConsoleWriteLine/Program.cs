ee054c23053f80eae071d43138df724f81c60d38
kubiszr25 <kubiszrudolf@gmail.com>
2025-09-24 09:42:49 +0200

    praca s git hubom var float bovfffo

--- src/AppsLab-002-ConsoleWriteLine/Program.cs
+++ src/AppsLab-002-ConsoleWriteLine/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int age = 16;
+string name = "Rudolf";
+
+Console.WriteLine(age);
+Console.WriteLine(name);
--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+int mojeCislo = 10;
+string mojeSlovo = "Ahoj , Svet!";
+bool mojaBoolHodnota = true;
+DateTime dnesnyDatum = DateTime.Now;
+float myFloat = 5.75f;
+int sucet = mojeCislo + 5;
+var myVar = "Toto je var string";
+Console.WriteLine(mojeCislo.ToString("777777"));
+Console.WriteLine(mojeSlovo);
+Console.WriteLine($"Súčet je:{sucet}");
+Console.WriteLine($"Dnes je: {dnesnyDatum.ToString("yyyy-MM-dd")}");
+Console.WriteLine($"Bool hodnota je: {mojaBoolHodnota}");
