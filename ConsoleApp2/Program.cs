68d89504c876838afa5a054e014d201eca011cb6
kubiszr25 <kubiszrudolf@gmail.com>
2025-10-08 07:57:41 +0200

    kalkulacka

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,11 +4,24 @@ Console.WriteLine("Zadaj druhe cislo: ");
 int cislo2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Zadaj operaciu(+,-,*,/)");
 var Operacia = Console.ReadLine();
-int sucet = cislo1 + cislo2;
-int rozideil = cislo1 - cislo2;
-
-
-
+int vysledok = 0;
+if (Operacia == "+")
+{
+    vysledok = cislo1 + cislo2;
+}
+else if (Operacia == "-")
+{
+     vysledok = cislo1 + cislo2;
+}
+else if (Operacia == "*")
+{
+     vysledok = cislo1 * cislo2;
+}
+else if (Operacia == "/")
+{
+     vysledok = cislo1 / cislo2;
+}
+Console.WriteLine($"vysledok je {vysledok}");
 
 
 
